b007908448b38089c447075f5d55ce60ab5868d3
Aaron Toliver <flux.charlotte@gmail.com>
2022-05-17 15:48:30 -0400

    user form

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -2,6 +2,7 @@ import { Model } from "./Model";
 import { Eventing } from './Eventing'
 import { Attributes } from './Attributes'
 import { ApiSync } from "./ApiSync";
+import { Collection } from "./Collection";
 
 
 export interface UserProps { // describe a custom type
@@ -13,12 +14,16 @@ export interface UserProps { // describe a custom type
 const rootUrl = 'http://localhost:3000/users'
 
 export class User extends Model<UserProps> {
-    static builduser(attrs: UserProps): User {
+    static buildUser(attrs: UserProps): User {
         return new User(
             new Attributes<UserProps>(attrs),
             new Eventing(),
             new ApiSync<UserProps>(rootUrl)
         )
+    }
+
+    static buildUserCollection(): Collection<User, UserProps> {
+        return new Collection<User, UserProps>(rootUrl, (json: UserProps) => User.buildUser(json))
     }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { User } from "./Models/User";
+import { UserForm  } from "./views/UserForm";
 
-const user = User.builduser({ id: 1, name: 'newest name',  age: 82 });
+const userForm = new UserForm(
+    document.getElementById('root')
+);
 
-user.on('change', () => {
-    console.log(user)
-});
-
-user.fetch();
+userForm.render()
